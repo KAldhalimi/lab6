51e0d973600dae4a42bdaab143ea2cd108d7a605
KAldhalimi <karrarmajed@gmail.com>
2019-01-18 06:56:14 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Casino!");
+            bool UserContinue = true;
+            while (UserContinue == true)
+            {
+
+                Console.WriteLine("Roll the dice? y/n");
+                string start = Console.ReadLine();
+                if (start.ToLower() == "no" || start.ToLower() == "n")
+                {
+                    UserContinue = false;
+                    continue;
+                }
+
+
+                Console.WriteLine("Enter number of sides of your dice ");
+                int sides = (int.Parse(Console.ReadLine()));
+
+                Console.WriteLine((RandomDie(sides)));
+                
+
+                Console.WriteLine("Do you want to play gain? y/n?");
+                string play = Console.ReadLine();
+
+                if (play.ToLower() == "no" || play.ToLower() == "n")
+                {
+                    UserContinue = false;
+                }
+
+            }
+
+
+        }
+
+
+        public static int RandomDie(int max)
+        {
+            
+            Random random = new Random();
+
+            int dice = random.Next(1, max + 1);
+            
+
+            return dice;
+
+
+
+        }
+
+
+        
+        
+            
+
+        }
+    }
+
+
